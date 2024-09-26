3a964f57626696d5bc7566c1040efac94aad7be7
uzairkhan1000 <uk5598060@gmail.com>
2024-09-27 00:14:54 +0500

    sidebar updates

--- app/Http/Livewire/Sidebar.php
+++ app/Http/Livewire/Sidebar.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
     public $activeView = 'dashboard';
+
+    public $sidebar_items = [
+        'Dashboard' => 'dashboard', 
+        'Product' => [
+            'Add Product' => 'product.add-product-form',
+            'All Product' => 'product.product-listing',
+        ],
+        'Order' => [
+            'Add Order' => 'order.add-order',
+            'All Order' => 'order.all-orders',
+        ],
+        'Roles / Permissions' => [
+            'Roles' => [
+                'Add Role' => 'roles.add-role',
+                'All Role' => 'roles.all-roles'
+            ],
+        ],
+    ];
 
     protected $listeners = ['setActiveMenuItem'];
 
@@ -24,5 +44,34 @@ class Sidebar extends Component
     public function setActiveMenuItem($view)
     {
         $this->activeView = $view;
+    }
+
+    public function getIconClass($component)
+    {
+        // Make an HTTP request to the Iconify API with the "fa" prefix
+        if (str_word_count($component) === 1) {
+            // If only one word, just convert to lowercase
+            $icon = 'fa-' . strtolower($component);
+        } else {
+            // If more than one word, explode and get the last word, then convert to lowercase
+            $words = explode(' ', $component);
+            $icon = 'fa-' . strtolower(end($words));
+        }
+
+        
+        $response = Http::get("https://api.iconify.design/search?query=$icon");
+        
+        if ($response->successful()) {
+            $iconData = $response->json();
+            Log::info( $iconData);
+            // Extract the first icon from the search results
+            $firstIcon = $iconData['icons'][0] ?? null;
+            if ($firstIcon) {
+                return str_replace(':', '-', $firstIcon);
+            }
+        }
+
+        // Handle error (e.g., icon not found)
+        return 'fa-question-circle'; // Default icon if not found
     }
 }
